a97d1437f9a3c27e1fe1405714c2fb75e7f379e3
Ding <wynterding@gmail.com>
2025-04-29 14:55:27 +0800

    support named export file (#23)
    * chore: 增加 nocacheRequire 导出 (#21)
    
    * fix: 支持具名导出 locale 文件

--- src/utils.js
+++ src/utils.js
@@ -5,4 +5,16 @@ Utils.noCacheRequire = function (resolvedPath) {
   return require(resolvedPath);
 };
 
+Utils.extractLocaleFromExport = function (data) {
+  // 处理各类场景下的 i18n 数据
+  const exportValue = data.default || data;
+  const firstValue = Object.values(exportValue)[0];
+
+  if (typeof firstValue === 'object') {
+    return firstValue;
+  }
+
+  return exportValue;
+};
+
 module.exports = Utils;
